4e7ccc106af21582148a0ecd7b66abf697c7d6fd
Christian Hundt <hundt@uni-mainz.de>
2013-03-25 21:11:57 +0100

    local file source

--- file_source/local_file_source.py
+++ file_source/local_file_source.py
@@ -1,19 +1,34 @@
-class abstract_file_source(object):
+from abstract_file_source import abstract_file_source 
+import os
+
+class local_file_source(abstract_file_source):
 
     def __init__(self, destination):
         """init file source with root node destination"""
         
-        raise Exception ("not implemented")
+        self.root = destination
 
     def list_content(self, basedir=None):
         """list content in basedir"""
         
-        raise Exception ("not implemented")
+        if basedir == None:
+            basedir = self.root
+        
+        os.chdir(basedir)
+        entries = os.listdir(basedir)
+        
+        files = filter(os.path.isfile, entries) 
+        folders = filter(os.path.isdir, entries)
+        
+        return basedir, folders, files
 
     def walk_content(self, basedir=None):
         """walk content from basedir"""
         
-        raise Exception ("not implemented")
+        if basedir == None:
+            basedir = self.root
+        
+        return list(os.walk(basedir))
     
     def get_meta_data(self, target, verbose=False):
         """get meta data dictionary from given target"""
@@ -27,8 +42,7 @@ class abstract_file_source(object):
         
 if __name__ == '__main__':
     
-    share = abstract_file_source("/home")
+    share = local_file_source("/home")
     print share.list_content("/tmp")
     print share.walk_content("/tmp")
-
 
