21f233509218ffed5dde8fc2e813f835e95676da
Christian Hundt <hundt@uni-mainz.de>
2013-03-25 19:23:12 +0100

    better a burning house than a silent house

--- file_source/abstract_file_source.py
+++ file_source/abstract_file_source.py
@@ -3,21 +3,24 @@ class abstract_file_source:
     def __init__(self, destination):
         """init file source with root node destination"""
         
-        pass
+        print("not implemented")
+        raise
 
     def walk_from(self, target=None, recursive=False):
         """walk from target node (os.walk like)"""
         
-        pass
+        print("not implemented")
+        raise
     
     def get_meta_data(self, target, verbose=False):
         """get meta data dictionary from given target"""
     
-        pass
+        print("not implemented")
+        raise
         
     def get_file_handler(self, offset=0):
         """get a file handler with offset"""
         
-        pass
-        
+        print("not implemented")
+        raise
 
