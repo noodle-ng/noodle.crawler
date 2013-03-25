493ae3f07d3ae8ed33ad861c8dda03b0973481de
Christian Hundt <hundt@uni-mainz.de>
2013-03-25 19:26:57 +0100

    better write valid code

--- file_source/abstract_file_source.py
+++ file_source/abstract_file_source.py
@@ -3,24 +3,24 @@ class abstract_file_source:
     def __init__(self, destination):
         """init file source with root node destination"""
         
-        print("not implemented")
-        raise
+        raise Exception ("not implemented")
 
     def walk_from(self, target=None, recursive=False):
         """walk from target node (os.walk like)"""
         
-        print("not implemented")
-        raise
+        raise Exception ("not implemented")
     
     def get_meta_data(self, target, verbose=False):
         """get meta data dictionary from given target"""
     
-        print("not implemented")
-        raise
+        raise Exception ("not implemented")
         
     def get_file_handler(self, offset=0):
         """get a file handler with offset"""
         
-        print("not implemented")
-        raise
+        raise Exception ("not implemented")
+        
+if __name__ == '__main__':
+    
+    burning_house = abstract_file_source(None)
 
