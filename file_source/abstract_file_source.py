9691cc893ce524337f21c0aaabc0817f4bc7449b
Moritz Schlarb <moschlar@metalabs.de>
2013-03-25 21:03:29 +0100

    Code cleanup as per PEP8

--- file_source/abstract_file_source.py
+++ file_source/abstract_file_source.py
@@ -1,26 +1,26 @@
-class abstract_file_source:
+# -*- coding: utf-8 -*-
+""""""
+
+class AbstractFileSource(object):
+    """"""
 
     def __init__(self, destination):
         """init file source with root node destination"""
-        
-        raise Exception ("not implemented")
+        raise NotImplementedError()
 
     def walk_from(self, target=None, recursive=False):
         """walk from target node (os.walk like)"""
-        
-        raise Exception ("not implemented")
-    
+        raise NotImplementedError()
+
     def get_meta_data(self, target, verbose=False):
         """get meta data dictionary from given target"""
-    
-        raise Exception ("not implemented")
-        
+        raise NotImplementedError()
+
     def get_file_handler(self, offset=0):
         """get a file handler with offset"""
-        
-        raise Exception ("not implemented")
-        
+        raise NotImplementedError()
+
 if __name__ == '__main__':
-    
-    burning_house = abstract_file_source(None)
+
+    burning_house = AbstractFileSource(None)
 
